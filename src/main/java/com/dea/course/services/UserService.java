2b3b9a8ff55caa6980ea8d5dc68a3c17e1098bfa
Eduardo Dea <eduardodea@outlook.com>
2023-02-17 02:22:56 -0300

    Service layer, component registration

---
+++ src/main/java/com/dea/course/services/UserService.java
@@ -0,0 +1,30 @@
+package com.dea.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dea.course.entities.User;
+import com.dea.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+	
+	
+}
