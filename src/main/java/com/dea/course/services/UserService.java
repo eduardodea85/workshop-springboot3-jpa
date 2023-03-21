fb88e73212400b61cd7a6c3790f7b6ff7ca233f9
eduardodea85 <eduardodea@outlook.com>
2023-03-20 21:06:25 -0300

    Exception handling - update

--- src/main/java/com/dea/course/services/UserService.java
+++ src/main/java/com/dea/course/services/UserService.java
@@ -16,6 +16,8 @@ import com.dea.course.repositories.UserRepository;
 import com.dea.course.services.exceptions.DatabaseException;
 import com.dea.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 //Implementando
 
 @Service //Essa anotation registra a classe como um serviço do spring e ela vai poder ser injetada automaticamente com o @Autowired
@@ -49,9 +51,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
